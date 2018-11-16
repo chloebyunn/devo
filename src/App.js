efde55c8f6ab3c634a454f308023de77cac6efba
chloebyunn <chloebyunn@gmail.com>
2018-11-16 13:52:01 -0500

    basic TitleCard component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.scss';
 
+import TitleCard from './component/TitleCard/TitleCard'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <TitleCard />    
       </div>
     );
   }
---
+++ src/component/TitleCard/TitleCard.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './TitleCard.scss';
+
+class TitleCard extends Component {
+    constructor() {
+        super();
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <div className="titlecard-container">
+                <input className="title-field" type="text" placeholder="Enter the name of this space" />                
+                <div className="name-fields">
+                    <p className="small-field">By:  </p>
+                    <input className="small-field" type="text" placeholder="Your Name" />
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default TitleCard;
