7d21318e1056b67e353af72a8ea3b19d21211063
chloebyunn <chloebyunn@gmail.com>
2018-11-16 14:00:27 -0500

    basic prevousentry setup

---
+++ src/component/PreviousEntry/PreviousEntry.js
@@ -0,0 +1,23 @@
+import {React, Component} from 'react';
+import './PreviousEntry.scss';
+
+class PreviousEntry extends Component { 
+    costructor() { 
+        super();
+        this.state = {
+
+        };
+    }
+
+    render() {
+
+
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default PreviousEntry;
