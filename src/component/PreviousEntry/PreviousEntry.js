c92eb34f4ef857c0a85c3a39690ba156a54b7e86
chloebyunn <chloebyunn@gmail.com>
2018-11-29 16:05:56 -0500

    using redux actions

--- src/component/PreviousEntry/PreviousEntry.js
+++ src/component/PreviousEntry/PreviousEntry.js
@@ -1,35 +1,45 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { deleteEntry } from '../../actions'
 import './PreviousEntry.scss';
+
 import PropTypes from 'prop-types'; 
 
 import share from '../../icons/share.png';
 import deleteicon from '../../icons/deleteicon.png';
 
+
 class PreviousEntry extends Component { 
+
+    handleDeleteClick = deleteEntryId => {
+        const { deleteEntry } = this.props;
+        deleteEntry(deleteEntryId);
+    };
 
     getNumDay() {
         return new Date().getDate();
-      }
+    }
     
-      getWeekday() {
+    getWeekday() {
         let weekday = new Date().getDay();
         const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Satday"]
         return dayNames[weekday];
-      }
+    }
     
-      getCurrentMonth() { 
+     getCurrentMonth() { 
         let mm = new Date().getMonth();
         const monthNames = ["January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"];
       
         return monthNames[mm];
-      }
+    }
     
-      getCurrentYear() {
+    getCurrentYear() {
         return new Date().getFullYear();
-      }
+    }
 
     render() {
+        const  { entryId, entry } = this.props;
         return (
             <div className="previous-container">
                 <div className="date-section">   
@@ -37,12 +47,13 @@ class PreviousEntry extends Component {
                     <div className="numday">{ this.getNumDay() }</div>
                 </div>
                 <div className="content-section">
-                    <div className="entry-title">Test title</div>
+                    <div className="entry-title">{entry.title}</div>
                     <div className="entry-content-preview">Test content</div>
                 </div>
                 <div className="hover-menu-items">
                     <img className="menu-icon" src={share} alt="share" />
-                    <img className="menu-icon" src={deleteicon} alt="X" />
+                    <img className="menu-icon" src={deleteicon} alt="X" 
+                         onClick={() => this.handleDeleteClick(entryId)}/>
                 </div>
             </div>
         );
